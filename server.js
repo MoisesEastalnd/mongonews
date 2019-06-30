11fc656930abe132c172395b231f9da903c3a8a3
moises eastland <dewaynepade95@gmail.com>
2019-06-30 18:48:44 -0500

    restarting my device this causious commit

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = ("mongoose");
+
+//scraping  tools 
 const axios = require("axios");
 const cheerio = ("cheerio");
-const mongoose = ("mongoose");
+
+//server port 
+const PORT = 6000
+
+const db = require("./models");
+
+const app = express();
